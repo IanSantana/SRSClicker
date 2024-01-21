192f35cdd75c3fefa872958dfad1e4097a25d9b7
Ian Sant'ana <42079086+IanSantana@users.noreply.github.com>
2024-01-21 17:02:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from dataclasses import replace
 from xmlrpc.client import boolean
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import os, time
 #login
-path = os.path.abspath('msedgedriver.exe')
-browser = webdriver.Edge(path)
+#path = os.path.abspath('msedgedriver.exe')
+browser = webdriver.Edge()
 browser.get('https://servicosredessociais.com.br/')
-usuario = browser.find_element_by_css_selector('#username')
+usuario = browser.find_element(By.CSS_SELECTOR,'#username')
 usuario.send_keys('usuario')
-usuario = browser.find_element_by_css_selector('#password') 
+usuario = browser.find_element(By.CSS_SELECTOR,'#password') 
 usuario.send_keys('senha')
 usuario.submit()
 #3572:13440:0205/144234.043:ERROR:chrome_browser_main_extra_parts_metrics.cc(251)
@@ -18,17 +22,25 @@ usuario.submit()
 j = 0
 valor = 1
 while(j < 11):
-    time.sleep(30)
     browser.get('https://servicosredessociais.com.br/orders/all/' + str(j))
     
         #preparaSearch = '/html/body/div/div/div/div/table/tbody/tr[valor]/td[10]/div/a'
         #preparaSearch = preparaSearch.replace('valor', str(valor))
         
-    target = browser.find_elements_by_partial_link_text("Reposição")
-    for x in range(0,len(target)):
-                if target[x].is_displayed():
-                    target[x].click()
+    target = browser.find_element(By.XPATH,"/html/body/div[2]/div/div/div[3]/table/thead/tr/th[13]/button")
+    target.click();
+    try:
+        WebDriverWait(browser, 3).until(EC.alert_is_present(),
+                                       'Timed out waiting for PA creation ' +
+                                       'confirmation popup to appear.')
+
+        alert = browser.switch_to.alert
+        alert.accept()
+        print("alert accepted")
+    except TimeoutException:
+        print("no alert")
         
+    time.sleep(30)
     j += 1
 
 browser.quit()
